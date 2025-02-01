955f2dd0d8de1d4aa442e10a9919f7da714b8813
ChetnaCodes <rajeshgodara0401@gmail.com>
2025-02-01 17:30:31 +0530

    Add files via upload

---
+++ Arithmetic_operators.java
@@ -0,0 +1,31 @@
+package Lecture1;
+
+public class Arithmetic_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b= 7;
+		int c = a+b;
+		System.out.println(c);
+		c= a-b;
+		System.out.println(c);
+		c = c+7;
+		System.out.println(c);  
+		c = a*b;
+		System.out.println(c);
+		c = a/b;
+		System.out.println(c);
+		c = b/a;
+		System.out.println(c);
+		c = b%a;
+		System.out.println(c);
+		c= a%b;
+		System.out.println(c);
+		
+		
+		
+
+	}
+
+}
